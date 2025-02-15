2f03aa8f1c5c985d0b5a3ee5288f5ec13cded012
Sakshi Patel <w0860745@myscc.ca>
2025-02-15 16:04:26 -0500

    Add basic daily challenge things.

---
+++ angularProject/src/app/daily-challenges/daily-challenges.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+
+
+interface Challenge{
+  id : number;
+  title : String,
+  type : String,
+  status : "pending" | "completed" | "skipped",
+  icon : String
+}
+
+@Component({
+  selector: 'app-daily-challenges',
+  standalone: true,
+  imports: [
+    NgClass,
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './daily-challenges.component.html',
+  styleUrl: './daily-challenges.component.css'
+})
+
+
+export class DailyChallengesComponent {
+
+  challengeList : Challenge[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() : void {
+    this.challengeList = [
+      {id : 1, title:"Do 10 Push ups", type:"Fitness", status:"pending", icon: "fa-solid fa-dumbbell"},
+      {id : 1, title:"Do 10 Push ups", type:"Fitness", status:"pending", icon: "fa-solid fa-dumbbell"},
+      {id : 1, title:"Do 10 Push ups", type:"Fitness", status:"pending", icon: "fa-solid fa-dumbbell"},
+      {id : 1, title:"Do 10 Push ups", type:"Fitness", status:"pending", icon: "fa-solid fa-dumbbell"},
+      {id : 1, title:"Do 10 Push ups", type:"Fitness", status:"pending", icon: "fa-solid fa-dumbbell"}
+
+    ]
+  }
+
+  markAsCompleted(challenge: Challenge) {
+    challenge.status = 'completed';
+  }
+
+  skipChallenge(challenge: Challenge) {
+    challenge.status = 'skipped';
+  }
+}
