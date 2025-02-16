e5c288812213d21f3dac46059025825a5bc8201b
Sakshi Patel <w0860745@myscc.ca>
2025-02-16 11:37:49 -0500

    Add routes but not working.

--- angularProject/src/main.ts
+++ angularProject/src/main.ts
@@ -3,10 +3,14 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from '@angular/router';
 import {DailyChallengesComponent} from './app/components/daily-challenges/daily-challenges.component';
+import {LeaderBoardComponent} from './app/components/leader-board/leader-board.component';
+import {HomeComponent} from './app/components/home/home.component';
 
 
 const routes: Routes = [
-  { path: 'challenges', component: DailyChallengesComponent },
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent }, // Default route (home)
+  { path: 'leaderboard', component: LeaderBoardComponent }, // Leaderboard page
 ];
 
 bootstrapApplication(AppComponent, {
